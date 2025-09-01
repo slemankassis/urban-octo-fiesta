f764963786d5f50199355638f3601cd18226e784
slemankassis <slemankassis@hotmail.com>
2025-09-01 14:29:14 -0300

    simple form and server

--- vite-project/src/server.js
+++ vite-project/src/server.js
@@ -3,6 +3,13 @@ import ViteExpress from "vite-express";
 
 const app = express();
 
-app.get("/message", (_, res) => res.send("Hello from express!"));
+const transactions = [];
+
+app.get("/transaction", (_, res) => res.send(transactions));
+app.post("/transaction", (req, res) => {
+  console.log("🚀 ~ req:", req);
+
+  res.send(transactions.push(req.body.transactions));
+});
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
